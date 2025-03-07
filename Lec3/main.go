da37a5c48a1b488bfd17acc60d3aec87b10e6bb4
armfel <armfel2010@gmail.com>
2025-03-07 22:11:15 +0300

    Modified main.go in Lec3

--- Lec3/main.go
+++ Lec3/main.go
@@ -7,17 +7,22 @@ import (
 
 func main() {
 	var (
-		age  int
-		name string
+		name    string
+		surname string
+		age     int
 	)
 
-	//fmt.Scan(&age)
 	//fmt.Scan(&name)
-	fmt.Scan(&age, &name)
+	//fmt.Scan(&surname)
+	//fmt.Scan(&age)
+	//fmt.Scan(&age, &name)
 
-	fmt.Printf("My name is: %s\nMy age is: %d\n", name, age)
+	//fmt.Printf("My name is: %s\nMy age is: %d\n", name, age)
 
 	//Для ручного использования потока
+	fmt.Fscan(os.Stdin, &name)
+	fmt.Fscan(os.Stdin, &surname)
 	fmt.Fscan(os.Stdin, &age)
-	fmt.Println("New age:", age)
+	//fmt.Println("New age:", age)
+	fmt.Printf("Имя: %s , Фамилия: %s , Возраст: %d . Студент BPS\n", name, surname, age)
 }
