d3327e88554fc0d54ac77b4fb15b5489ad61f6f5
armfel <armfel2010@gmail.com>
2025-03-10 18:26:55 +0300

    Modified main.go in Lec5

--- Lec5/main.go
+++ Lec5/main.go
@@ -47,4 +47,38 @@ func main() {
 	} else {
 		fmt.Println("Unknown color")
 	}
+
+	//Good Инициализация в блоке условного оператора
+	//Блок присваявания - только :=
+	//Инициализируемая переменная видна только внутри области видимости условного оператора (в телах if, else if, else)
+	// Но не за его пределами
+	if num := 10; num%2 == 0 {
+		fmt.Println("EVEN")
+	} else {
+		fmt.Println("ODD")
+	}
+
+	//Ущербно
+	/*var age int = 10
+	if age > 7 {
+		fmt.Println("Go to school")
+	} //По факту, сюда подставляется ; компилятором
+	else {
+		fmt.Println("Another case")
+	}
+	*/
+
+	//НЕ ИДЕОМАТИЧНО
+	if width := 100; width > 100 {
+		fmt.Println("Width > 100")
+	} else {
+		fmt.Println("Width <= 100")
+	}
+	//Странное правило номер 1: в Go стараются избегать блоков else
+	//Идеоматичность
+	if height := 100; height > 100 {
+		fmt.Println("height > 100")
+		return
+	}
+	fmt.Println("height <= 100")
 }
